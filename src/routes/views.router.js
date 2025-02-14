97ff812ea3928a33419f2cac13b887315039cd8f
Mai Rodrigo <maialrodrigo@gmail.com>
2025-02-14 00:31:15 -0300

    Add: home with all products

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,9 +1,16 @@
 import express from "express";
+import ProductManager from "../managers/ProductManager.js";
 
 const viewsRouter = express.Router();
+const productsManager = new ProductManager("./src/data/products.json");
 
-viewsRouter.get("/", (req, res) => {
-	res.render("home", {});
+viewsRouter.get("/", async (req, res) => {
+	try {
+		const products = await productsManager.getProducts();
+		res.render("home", { products });
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
 });
 
 export default viewsRouter;
