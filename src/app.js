c0a585e01081d7212e40a5c913427bbfee4fdc71
Mai Rodrigo <maialrodrigo@gmail.com>
2025-02-14 22:25:30 -0300

    Add: first configuration of realtimeproducts

--- src/app.js
+++ src/app.js
@@ -23,32 +23,8 @@ app.set("views", "./src/views");
 const PORT = 8080;
 // Middleware para procesar datos JSON en las solicitudes
 app.use(express.json());
-
-/* //Configuración de mensaje de bienvenida
-app.get("/", (req, res) => {
-	res.send(`
-    <h1>Bienvenido a Respira 🌿</h1>
-    <p>Proyecto de tienda de productos energéticos.</p>
-    <h2>📌 Rutas disponibles:</h2>
-    <ul>
-    <li><strong>Productos:</strong></li>
-    <ul>
-        <li>📍 <strong>GET</strong> <a href="/api/products">/api/products</a> → Obtener todos los productos</li>
-        <li>📍 <strong>GET</strong> /api/products/:pid → Obtener un producto por ID</li>
-        <li>📍 <strong>POST</strong> /api/products → Agregar un nuevo producto</li>
-        <li>📍 <strong>PUT</strong> /api/products/:pid → Modificar un producto</li>
-        <li>📍 <strong>DELETE</strong> /api/products/:pid → Eliminar un producto</li>
-    </ul>
-    <li><strong>Carritos:</strong></li>
-    <ul>
-        <li>📍 <strong>POST</strong> <a href="/api/carts">/api/carts</a> → Crear un nuevo carrito</li>
-        <li>📍 <strong>GET</strong> /api/carts/:cid → Obtener un carrito por ID</li>
-        <li>📍 <strong>POST</strong> /api/carts/:cid/products/:pid → Agregar un producto a un carrito</li>
-    </ul>
-    </ul>
-    <p>Prueba estas rutas en Postman o directamente en el navegador.</p>
-`);
-}); */
+//Habilitacion de la carpeta public
+app.use(express.static("public"));
 
 // Configuración de rutas principales
 app.use("/api/products", productsRouter);
@@ -58,6 +34,11 @@ app.use("/", viewsRouter);
 //Manejo de rutas no encontradas
 app.use((req, res) => {
 	res.status(404).json({ error: "Ruta no encontrada" });
+});
+
+//Websockets
+io.on("connection", (socket) => {
+	console.log("Ingresó un nuevo usuario");
 });
 
 //Se levanta el servidor
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -4,10 +4,21 @@ import ProductManager from "../managers/ProductManager.js";
 const viewsRouter = express.Router();
 const productsManager = new ProductManager("./src/data/products.json");
 
+// Renderiza la vista principal con todos los productos
 viewsRouter.get("/", async (req, res) => {
 	try {
 		const products = await productsManager.getProducts();
 		res.render("home", { products });
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+});
+
+// Renderiza la vista con WebSocket
+viewsRouter.get("/realtimeproducts", async (req, res) => {
+	try {
+		const products = await productsManager.getProducts();
+		res.render("realTimeProducts", { products });
 	} catch (error) {
 		res.status(500).send({ message: error.message });
 	}
